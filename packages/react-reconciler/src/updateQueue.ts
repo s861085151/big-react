e7ae4c9c820d12e67b6639a22a7f58fc94c09083
qiang_song <qiang.song@databurning.com>
2023-10-31 10:35:10 +0800

    feat: updateQueue

---
+++ packages/react-reconciler/src/updateQueue.ts
@@ -0,0 +1,53 @@
+import { Action } from 'shared/ReactTypes';
+
+export interface Update<State> {
+	action: Action<State>;
+}
+
+export const createUpdate = <State>(action: Action<State>): Update<State> => {
+	return {
+		action
+	};
+};
+
+export interface UpdateQueue<State> {
+	shared: {
+		pending: Update<State> | null;
+	};
+}
+
+export const createUpdateQueue = <Action>() => {
+	return {
+		shared: {
+			pending: null
+		}
+	} as UpdateQueue<Action>;
+};
+
+export const enqueueUpdate = <Action>(
+	updateQueue: UpdateQueue<Action>,
+	update: Update<Action>
+) => {
+	updateQueue.shared.pending = update;
+};
+
+export const processUpdateQueue = <State>(
+	baseState: State,
+	pendingUpdate: Update<State> | null
+): { memoizedState: State } => {
+	const result: ReturnType<typeof processUpdateQueue<State>> = {
+		memoizedState: baseState
+	};
+
+	if (pendingUpdate !== null) {
+		const action = pendingUpdate.action;
+		// baseState 1 update (x) => 4x -> memoizedState 4
+		if (action instanceof Function) {
+			result.memoizedState = action(baseState);
+		} else {
+			// baseState 1 update 2 -> memoizedState 2
+			result.memoizedState = action;
+		}
+	}
+	return result;
+};
